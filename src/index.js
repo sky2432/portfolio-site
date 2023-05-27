6eaf099525dfbcd5a348351aab26576596aa9a71
sky2432 <skyarts0122@gmail.com>
2023-05-27 12:37:40 +0900

    Add GA4

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue';
 import router from './router/index';
-import App from './App.vue'
+import App from './App.vue';
 import 'ress';
+import VueGtag from 'vue-gtag';
 
 const app = createApp(App);
 app.use(router);
+app.use(
+  VueGtag,
+  {
+    config: { id: process.env.VUE_APP_GA_MEASUREMENT_ID },
+  },
+  router
+);
 app.mount('#app');
